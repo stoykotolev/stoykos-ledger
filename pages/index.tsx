319d5e9492efd6d2b141ed16226d6f05feb4f1cf
Stoyko Tolev <stoyko@rodeo.bg>
2021-12-04 18:33:58 +0200

    Update: Layout the groundwork for the home page. Configure a bit of the typography.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,32 @@ import Layout from '../components/layout';
 const HomePage = () =>
   (
     <Layout>
-      <h1>This is my website</h1>
+      <header id='header-section' className='h1'>
+        <h3>Welcome to</h3>
+        <h1 className='page-title'>Stoyko&apos;s Ledger</h1>
+      </header>
+      <section id='intro-section'>
+        <p>
+          {/* <h2>My personal website.</h2> */}
+          It will server as both a continious project and a ledger, where I can
+          keep notes and present how I come up with project ideas, the process in
+          building such a project, deploying it and anything inbetween.
+        </p>
+      </section>
+      <section id='about-me'>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, fugit
+          magni porro voluptatum iste totam deserunt voluptas, deleniti a itaque
+          exercitationem. Necessitatibus eveniet repellat recusandae inventore
+          voluptate voluptatem, possimus est!
+        </p>
+      </section>
+      <section id='contact-me'>
+        <p>
+          If you want to share something, are in need of a developer or have an
+          interesting idea in mind, hit me up. I&apos;ll be happy to hear from you.
+        </p>
+      </section>
     </Layout>
   );
 
