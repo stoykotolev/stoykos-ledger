005ee5192faf9e9413bf975dd207a5b1a4349a43
Stoyko Tolev <st.tolev@gmail.com>
2026-03-09 17:00:21 +0200

    misc(*): Setup eslint

---
+++ eslint.config.js
@@ -0,0 +1,6 @@
+import eslintPluginAstro from "eslint-plugin-astro";
+export default [
+  // add more generic rule sets here, such as:
+  // js.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+];
